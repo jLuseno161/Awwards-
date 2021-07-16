767c484d42d914b340bfff22ac51172013269f51
jLuseno161 <joyluseno2@gmail.com>
2021-07-16 17:52:35 +0300

    save

--- awwards/models.py
+++ awwards/models.py
@@ -9,6 +9,7 @@ from django.db.models.signals import post_save
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email_confirmed = models.BooleanField(default=False)
     bio = models.TextField(max_length=500, blank=True)
     location = models.CharField(max_length=30, blank=True)
     birth_date = models.DateField(null=True, blank=True)
