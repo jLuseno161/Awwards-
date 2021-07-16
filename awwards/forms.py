90d9fba101a0cdc5d1bea3551614b8bf5b3a683e
jLuseno161 <joyluseno2@gmail.com>
2021-07-16 17:30:49 +0300

    create user profile & using signals

--- awwards/forms.py
+++ awwards/forms.py
@@ -3,10 +3,9 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    full_name = forms.CharField(max_length=50, required=False)
     email = forms.EmailField()
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
+        fields = ('full_name', 'username','email', 'password1', 'password2', )
--- awwards_prj/settings.py
+++ awwards_prj/settings.py
@@ -127,3 +127,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
