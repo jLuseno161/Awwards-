8425a2d40340e4974d5edd062919ef2e3149e665
jLuseno161 <joyluseno2@gmail.com>
2021-07-16 21:30:49 +0300

    create profile model class

--- awwards/models.py
+++ awwards/models.py
@@ -2,18 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from django.db.models.signals import post_save
-
-
+from cloudinary.models import CloudinaryField
+from cloudinary.models import CloudinaryField
 
 
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    email_confirmed = models.BooleanField(default=False)
+    profile_pic = CloudinaryField('images')
+    # email_confirmed = models.BooleanField(default=False)
     bio = models.TextField(max_length=500, blank=True)
-    location = models.CharField(max_length=30, blank=True)
-    birth_date = models.DateField(null=True, blank=True)
-    
+    location = models.CharField(max_length=30, blank=True)  
 
 @receiver(post_save, sender=User)
 def update_user_profile(sender, instance, created, **kwargs):
