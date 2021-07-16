1026c22961d0eb18ba0019065a667efb3a88bc0d
jLuseno161 <joyluseno2@gmail.com>
2021-07-16 16:00:59 +0300

    create signup forms

--- awwards/views.py
+++ awwards/views.py
@@ -1,3 +1,10 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+@login_required(login_url='/accounts/login/')
+def index(request):
+ 
+  
+    return HttpResponse('Hi there my name is Joy')
--- awwards_prj/urls.py
+++ awwards_prj/urls.py
@@ -1,3 +1,17 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+
+# Create your views here.
+
+# @login_required(login_url='/accounts/login/')
+# def index(request):
+ 
+  
+#     return HttpResponse("Hi there my name is Joy")
+
+
 """awwards_prj URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -13,9 +27,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
+from django.urls import path
+
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('',include('awwards.urls')),  
 ]
