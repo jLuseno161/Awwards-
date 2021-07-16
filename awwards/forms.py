95f540c0d83515ec87f804238c2930e4db15caaf
jLuseno161 <joyluseno2@gmail.com>
2021-07-16 16:11:24 +0300

    authentication functions

--- awwards/forms.py
+++ awwards/forms.py
@@ -3,6 +3,8 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 
 class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
     email = forms.EmailField()
 
     class Meta:
