7eccbc0b352da4e9849a58174bc2f154cfc82516
jLuseno161 <joyluseno2@gmail.com>
2021-07-19 09:03:29 +0300

    create project tests

--- awwards/models.py
+++ awwards/models.py
@@ -5,36 +5,57 @@ from django.db.models.signals import post_save
 from cloudinary.models import CloudinaryField
 from cloudinary.models import CloudinaryField
 
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_pic = CloudinaryField('images')
     bio = models.TextField(max_length=500, blank=True)
-    contact = models.CharField(max_length=30, blank=True)  
+    contact = models.CharField(max_length=30, blank=True)
     # email_confirmed = models.BooleanField(default=False)
 
-@receiver(post_save, sender=User)
-def update_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-    instance.profile.save()
+    @receiver(post_save, sender=User)
+    def update_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+        instance.profile.save()
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    def update_profile(cls, id):
+        Project.objects.get(user_id=id)
+
 
 class Project(models.Model):
-    user = models.ForeignKey(User,on_delete = models.CASCADE, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     title = models.CharField(max_length=150)
     image = CloudinaryField('images')
-    profile = models.ForeignKey(Profile,on_delete = models.CASCADE,null=True)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True)
     url = models.URLField(blank=True)
-    description = models.TextField(max_length=300, blank=True)  
+    description = models.TextField(max_length=300, blank=True)
     date = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.title
+
+    def save_project(self):
+        return self.save()
+
+    def delete_project(self):
+        self.delete()
+
     @classmethod
-    def project_by_id(cls,id):
-        project = Project.objects.filter(id =id)
+    def project_by_id(cls, id):
+        project = Project.objects.filter(id=id)
         return project
 
     @classmethod
     def search_project(cls, name):
         return cls.objects.filter(title__icontains=name).all()
+
 
 class Rates(models.Model):
     RATE_CHOICES = (
@@ -49,10 +70,12 @@ class Rates(models.Model):
         (9, '9'),
         (10, '10'),
     )
-    user = models.ForeignKey(User,on_delete = models.CASCADE, null=True)
-    project = models.ForeignKey(Project,null=True,on_delete=models.CASCADE)
-    design = models.IntegerField(choices=RATE_CHOICES,default=0,blank=False)
-    usability = models.IntegerField(choices=RATE_CHOICES,default=0,blank=False)
-    content = models.IntegerField(choices=RATE_CHOICES,default=0,blank=False)
-    average =  models.DecimalField(default=1,blank=False,decimal_places=2,max_digits=40)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    project = models.ForeignKey(Project, null=True, on_delete=models.CASCADE)
+    design = models.IntegerField(choices=RATE_CHOICES, default=0, blank=False)
+    usability = models.IntegerField(
+        choices=RATE_CHOICES, default=0, blank=False)
+    content = models.IntegerField(choices=RATE_CHOICES, default=0, blank=False)
+    average = models.DecimalField(
+        default=1, blank=False, decimal_places=2, max_digits=40)
     date = models.DateTimeField(auto_now_add=True)
